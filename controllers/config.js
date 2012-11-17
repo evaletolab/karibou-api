ca7382d059c6932497bfd884413c897caa05504c
evaleto <evaleto@gmail.com>
2012-11-17 12:02:41 +0100

    add mongoose-auth

--- controllers/config.js
+++ controllers/config.js
@@ -1,29 +1,31 @@
   
 module.exports = function(app) {
 
-	var mongoose = require('mongoose');
-	var mongooseAuth = require('mongoose-auth');
-	var everyauth = require('everyauth')
-		, Promise = everyauth.Promise;
+  var mongoose = require('mongoose');
+  var mongooseAuth = require('mongoose-auth');
+  var everyauth = require('everyauth')
+    , Promise = everyauth.Promise;
 
   var home = require('./home');
+  var api = require('./index');
   var products = require('./products');
 
 
-	//
-	// auth logout
-	app.get('/logout', function (req, res) {
-		  req.logout();
-		  res.redirect('/');
-	});
+  //
+  // auth logout
+  app.get('/logout', function (req, res) {
+      req.logout();
+      res.redirect('/');
+  });
 
-  app.get('/v1', home.index);
-	app.get('/v1/products',products.list);
-	app.get('/v1/products/:id',products.get);
-	app.delete('/v1/products',products.mass_remove);
-	app.delete('/v1/products/:id', products.remove);
-	app.post('/v1/products',products.create);
-	app.put('/v1/products',products.mass_update);
-	app.put('/v1/products/:id',products.update);
-	
+  app.get('/', home.index);
+  app.get('/v1', api.index);
+  app.get('/v1/products',products.list);
+  app.get('/v1/products/:id',products.get);
+  app.delete('/v1/products',products.mass_remove);
+  app.delete('/v1/products/:id', products.remove);
+  app.post('/v1/products',products.create);
+  app.put('/v1/products',products.mass_update);
+  app.put('/v1/products/:id',products.update);
+  
 };
