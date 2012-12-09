451a169419eaae33580e86169c072ce6f353cee6
evaleto <evaleto@gmail.com>
2012-12-09 15:27:28 +0100

    tests products and categories

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ var app = require('./app/index');
 
 
 // launch server
+var port = (process.env.VMC_APP_PORT || process.env.C9_PORT || 3000);
+var host = (process.env.VMC_APP_HOST || 'localhost');
 
-app.listen(3000);
+//
+// manage c9 env
+if (process.env.C9_PORT ){
+    host='0.0.0.0';
+}
+
+
+app.listen(port);
 
