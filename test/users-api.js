586bc1c01cef07b6d2a2fd6edc8f717929012cd3
evaleto <evaleto@gmail.com>
2012-12-10 19:39:12 +0100

    more products implementation

--- test/users-api.js
+++ test/users-api.js
@@ -1,8 +1,6 @@
 // Use a different DB for tests
 var app = require("../app/index");
 
-var mongoose = require("mongoose");
-var Users = mongoose.model('Users');
 
 // why not using
 // https://github.com/1602/jugglingdb
@@ -15,29 +13,7 @@ describe("Users API", function(){
   var assert = require("assert");
   var request= require('supertest');
 
-  beforeEach(function(done){
 
-  	// create a new user
-    var user=new Users({
-		    provider:"twitter",
-		    id:312528659,
-		    photo:"https: //si0.twimg.com/profile_images/1385850059/oli-avatar-small_normal.png"
-    });
-
-    user.save(function(err){
-      profile=user;
-      profile.id.should.equal(312528659);
-	    done();
-    });
-
-  });
-
-  afterEach(function(done){
-    Users.remove({}, function(o) {
-      done();
-    });
-  });
-  
 
   it('GET /v1/users/me should return 401',function(done){
     request(app)
