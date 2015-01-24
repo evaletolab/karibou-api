3d58b1aa772162c60eec6c73579ae214e91183c8
evaletolab <evaleto@gmail.com>
2015-01-24 11:12:08 +0100

    fix bugs, implment repport, implement collect

---
+++ test/api.orders.repport.js
@@ -0,0 +1,95 @@
+var app = require("../app");
+
+
+var db = require('mongoose');
+var dbtools = require("./fixtures/dbtools");
+var should = require("should");
+var _ = require("underscore");
+var request= require('supertest');
+var data = dbtools.fixtures(["Users.js","Categories.js","Orders.find.js"]),
+    Orders=db.model('Orders');
+
+describe("api.orders.find", function(){
+  before(function(done){
+
+    dbtools.clean(function(e){
+      dbtools.load(["../fixtures/Users.js","../fixtures/Orders.repport.js"],db,function(err){
+        should.not.exist(err);
+
+        // Orders.printInfo()
+        //  Orders.find({}).exec(function(e,os){
+        //    os.forEach(function(o){o.print()})
+        //  })
+
+        done();
+      });
+    });      
+  });
+
+  
+  after(function(done){
+    dbtools.clean(function(){    
+      done();
+    });    
+  });
+
+  //
+  // keep session
+  var cookie;
+
+  it("login",function(done){
+    request(app)
+      .post('/login')
+      .send({ email: "evaleto@gmail.com", password:'password',provider:'local' })
+      .end(function(e,res){
+        should.not.exist(e)
+        cookie = res.headers['set-cookie'];
+        done()
+      });
+  })
+
+  it('GET /v1/orders should return 401 for anonymous',function(done){
+    request(app)
+      .get('/v1/orders')
+      .expect(401,done);
+  });
+
+
+  it('GET /v1/orders/invoices/shops/12/2014  list all open orders for admin',function(done){
+    request(app)
+      .get('/v1/orders/invoices/shops/12/2014')
+      .set('cookie', cookie)      
+      .expect(200,function(err,res){
+        should.not.exist(err)
+        console.log(res.body.shops)
+        //res.body.length.should.equal(3)
+        done()
+      });  
+  });  
+
+
+  it('GET /v1/orders/invoices/shops/12/2014?shops=crocorient,les-fromages-de-gaetan  list all open orders for admin',function(done){
+    request(app)
+      .get('/v1/orders/invoices/shops/12/2014?shops=crocorient,les-fromages-de-gaetan')
+      .set('cookie', cookie)      
+      .expect(200,function(err,res){
+        should.not.exist(err)
+        done()
+      });  
+  });  
+
+  it('GET /v1/orders/invoices/shops/12/2014  list all open orders for admin',function(done){
+    request(app)
+      .get('/v1/orders/invoices/shops/12/2014')
+      .expect(401,function(err,res){
+        should.not.exist(err)
+        //res.body.length.should.equal(3)
+        done()
+      });  
+  });  
+
+
+
+
+});
+
---
+++ test/order.logistic.js
@@ -0,0 +1,127 @@
+var app = require("../app");
+
+
+var db = require('mongoose');
+var dbtools = require("./fixtures/dbtools");
+var should = require("should");
+var _ = require("underscore");
+var request= require('supertest');
+var when16=new Date('Tue Dec 16 2014 16:00:00 GMT+0100 (CET)'),
+    when19=new Date('Fri Dec 19 2014 16:00:00 GMT+0100 (CET)'),
+    when23=new Date('Tue Dec 23 2014 16:00:00 GMT+0100 (CET)'),
+    Orders=db.model('Orders');
+
+describe("api.orders.find", function(){
+  before(function(done){
+
+    dbtools.clean(function(e){
+      dbtools.load(["../fixtures/Users.js","../fixtures/Orders.logistic.js"],db,function(err){
+        should.not.exist(err);
+
+        //Orders.printInfo()
+         // Orders.find({}).exec(function(e,os){
+         //   os.forEach(function(o){o.print()})
+         // })
+
+        done();
+      });
+    });      
+  });
+
+  
+  after(function(done){
+    dbtools.clean(function(){    
+      done();
+    });    
+  });
+
+  it('mark order shipped get an error when missing param',function(done){
+    Orders.updateLogistic({oid:2100000}, {},function (err, orders) {
+      should.exist(err)
+      err.should.include('Ooops error updateLogistic missing param')
+      done()
+    })
+  });
+
+  it('mark order shipped get an error when missing param',function(done){
+    Orders.updateLogistic({}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Ooops error updateLogistic missing order selector')
+      done()
+    })
+  });
+
+
+  it('mark order shipped get an error when payment is not auth',function(done){
+    Orders.updateLogistic({oid:2000006}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Impossible de livrer une commande sans validation financière')
+      done()
+    })
+  });
+
+  it('mark order shipped get an error when order is not fulfilled',function(done){
+    Orders.updateLogistic({oid:2000016}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Impossible de livrer une commande avec le status')
+      done()
+    })
+  });
+
+  it('mark order shipped get an error for wrong order id',function(done){
+    Orders.updateLogistic({oid:3000020}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Impossible de trouver la commande')
+      done()
+    })
+  });
+
+  it('mark order shipped true',function(done){
+    Orders.updateLogistic({oid:2000007}, {status:true},function (err, orders) {
+      should.not.exist(err)
+      should.exist(orders)
+      orders[0].shipping.shipped.should.be.true
+      done()
+    })
+  });
+
+
+  it('mark order collected get an error when missing date',function(done){
+    Orders.updateLogistic({'vendors.slug':'un-autre-shop'}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Ooops error updateLogistic missing date')
+      done()
+    })
+  });
+
+  it('mark order collected get an error if one order has issue',function(done){
+    var when=Orders.findCurrentShippingDay();
+    Orders.updateLogistic({'vendors.slug':'un-autre-shop'}, {status:true, when:when},function (err, orders) {
+      should.exist(err)
+      err.should.include('Impossible de livrer une commande')
+      done()
+    })
+  });
+
+  it.skip('mark order collect even when some orders are closed',function(done){
+    Orders.updateLogistic({oid:2100000}, {status:true},function (err, orders) {
+      should.exist(err)
+      err.should.include('Impossible de livrer une commande fermée')
+      done()
+    })
+  });
+
+  it('mark order collected ',function(done){
+    var when=Orders.findCurrentShippingDay();
+    Orders.updateLogistic({'vendors.slug':'un-autre-shop-2'}, {status:true, when:when},function (err, orders) {
+      should.not.exist(err)
+      should.exist(orders)
+      orders[0].vendors[0].collected.should.be.true
+      orders[0].vendors[1].collected.should.be.false
+      //orders[0].shipping.shipped.should.be.true
+      done()
+    })
+  });
+
+});
+
