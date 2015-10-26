3c70c3806db1b2b0ef0f56fdb7c08fa2fe9bf5e5
evaletolab <evaleto@gmail.com>
2015-10-26 17:02:06 +0100

    #40 initial commit for Documents

---
+++ app/bus.routes.activity.js
@@ -0,0 +1,39 @@
+var Activity = require('mongoose').model('Activities'),
+    Q=require('q');
+
+var log=exports.log=function(who, what, content, cb) {
+  var deferred = Q.defer();
+
+  Activity.create(who,what,content,function (err,result) {
+
+    if(err){
+      if(cb){return cb(err);}
+      return deferred.reject(err);
+    }
+    if(cb){
+      return cb(0,result)
+    }
+    deferred.resolve(result);
+  })
+
+  //
+  // flexible use of cb or promise
+  return  deferred.promise;
+}      
+      
+exports.create=function(who, what, content, cb) {
+  what.action='create';
+  return log(who,what,content,cb);
+};
+
+exports.update=function(who, what, content, cb) {
+  what.action='update';
+  return log(who,what,content,cb);
+};
+
+exports.delete=function(who, what, content, cb) {
+  what.action='delete';
+  return log(who,what,content,cb);
+};
+
+
