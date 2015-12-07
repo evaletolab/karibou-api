3b1c99bd303d4ca8f6b9c43adb6f38fc299c9588
evaletolab <evaleto@gmail.com>
2015-12-07 19:45:40 +0100

    config home from db

--- config/config-shop.js
+++ config/config-shop.js
@@ -8,13 +8,6 @@ module.exports = {
       post:{limitMS:500}
     },
 
-    //
-    // select display on home
-    bundle:{
-      path:'test-de-publication',
-      love:false
-    },
-
     document:{
       types:['recipe','post','bundle','page']
     },
