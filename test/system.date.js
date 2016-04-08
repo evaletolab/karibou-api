fb8093a9c4d2f3f9011de537a5deea10bdd56370
evaletolab <evaleto@gmail.com>
2016-04-08 11:27:49 +0200

    #52 refactor shipping date api

--- test/system.date.js
+++ test/system.date.js
@@ -35,7 +35,7 @@ describe("system.date", function(){
     config.shared.order.weekdays=weekdays;
     done()
   });
-  return;
+
   // Pour préparer une commande il faut X heures (disons 48h) 
   // Cela veut dire, qu'il faut deux matinées pour préparer la commande sachant 
   // que la deuxième matinée sera le jour de collecte. Donc limité à 10:00 du matin. 
@@ -62,16 +62,16 @@ describe("system.date", function(){
 
   it("[SELLER] with a time limit at 1:00AM the current shipping day for seller is today (except for sunday+1)", function(done){
     //if today time is > config.shared.order.timelimitH ==> go to next day
-    config.shared.order.timelimitH=1
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimitH=1;
+    var today=new Date(), nextSeller=Date.currentShippingDay();
     //console.log('------> today %d, nextSeller day %d',today.getDay(),nextSeller.getDay())
 
     //
     // sunday is off OR our time limit is 23h
     if (today.getDay()==0||hours===23){
-      nextSeller.getDay().should.equal((today.getDay()+1)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+1)%7);      
     }else{
-      nextSeller.getDay().should.equal(today.getDay()+0)      
+      nextSeller.getDay().should.equal(today.getDay()+0);      
     }
     console.log("TODO check if current date is correct order.date.js#L76")
 
@@ -80,16 +80,16 @@ describe("system.date", function(){
 
 
   it("[CUSTOMER] with a preparation time limit of 24H before 23:00 the current shipping day is today+1 (except for saturday+2)", function(done){
-    config.shared.order.timelimit=24
-    config.shared.order.timelimitH=23
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimit=24;
+    config.shared.order.timelimitH=23;
+    var today=new Date(), nextSeller=Date.nextShippingDay();
 
     //
     // sunday is off 
     if (today.getDay()==6||hours===23){
-      nextSeller.getDay().should.equal((today.getDay()+2)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+2)%7);      
     }else{
-      nextSeller.getDay().should.equal(today.getDay()+1)      
+      nextSeller.getDay().should.equal(today.getDay()+1);      
     }
 
     done();          
@@ -97,32 +97,34 @@ describe("system.date", function(){
 
 
   it("[CUSTOMER] with a preparation time limit of 24H before 1:00 the current shipping day is today+2 (except for friday+3)", function(done){
-    config.shared.order.timelimit=24
-    config.shared.order.timelimitH=1
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimit=24;
+    config.shared.order.timelimitH=1;
+    var today=new Date(), nextSeller=Date.nextShippingDay();
+
 
     //
     // sunday is off 
     if (today.getDay()==5){
-      nextSeller.getDay().should.equal((today.getDay()+3)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+3)%7);      
     }else{
-      nextSeller.getDay().should.equal((today.getDay()+2)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+2)%7);      
     }
 
     done();          
   });  
 
+
   it("[CUSTOMER] with a preparation time limit of 48H before 23:00 the current shipping day is today+2 (except for friday+3)", function(done){
-    config.shared.order.timelimit=48
-    config.shared.order.timelimitH=23
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimit=48;
+    config.shared.order.timelimitH=23;
+    var today=new Date(), nextSeller=Date.nextShippingDay();
 
     //
     // sunday is off BUT if current test time is > 23 then computed value is wrong
     if (today.getDay()==5||hours===23){
-      nextSeller.getDay().should.equal((today.getDay()+3)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+3)%7);      
     }else{
-      nextSeller.getDay().should.equal((today.getDay()+2)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+2)%7);      
     }
 
     done();          
@@ -130,50 +132,50 @@ describe("system.date", function(){
 
 
   it("[CUSTOMER] with a preparation time limit of 48H before 1:00 the current shipping day is today+3 (except for wednesday+4)", function(done){
-    config.shared.order.timelimit=48
-    config.shared.order.timelimitH=1
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimit=48;
+    config.shared.order.timelimitH=1;
+    var today=new Date(), nextSeller=Date.nextShippingDay();
 
     //
     // sunday is off 
     if (today.getDay()==4){
-      nextSeller.getDay().should.equal((today.getDay()+4)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+4)%7);      
     }else{
-      nextSeller.getDay().should.equal((today.getDay()+3)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+3)%7);      
     }
 
     done();          
   });
 
   it("[CUSTOMER] with a preparation time limit of 49H before 1:00 the current shipping day is today+4 (except for tuesday+5)", function(done){
-    config.shared.order.timelimit=49
-    config.shared.order.timelimitH=1
-    var today=new Date(), nextSeller=Date.currentShippingDay()
+    config.shared.order.timelimit=49;
+    config.shared.order.timelimitH=1;
+    var today=new Date(), nextSeller=Date.nextShippingDay();
 
     //
     // sunday is off 
     if (today.getDay()==4){
-      nextSeller.getDay().should.equal((today.getDay()+4)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+4)%7);      
     }else{
-      nextSeller.getDay().should.equal((today.getDay()+3)%7)      
+      nextSeller.getDay().should.equal((today.getDay()+3)%7);      
     }
 
     done();          
   });
 
   it("[CUSTOMER] one week of shipping days", function(done){
-    config.shared.order.timelimit=24
-    config.shared.order.timelimitH=1
-    var today=new Date(), all=Date.fullWeekShippingDays()
+    config.shared.order.timelimit=24;
+    config.shared.order.timelimitH=1;
+    var today=new Date(), all=Date.fullWeekShippingDays();
 
-    config.shared.order.timelimit.should.not.be.above((all[0].getTime()-Date.now())/3600000)
+    config.shared.order.timelimit.should.not.be.above((all[0].getTime()-Date.now())/3600000);
 
-    parseFloat((all[all.length-1].getTime()-all[0].getTime())/86400000).should.be.below(7)
+    parseFloat((all[all.length-1].getTime()-all[0].getTime())/86400000).should.be.below(7);
 
     all.forEach(function(n,i){
       // deprecated use containEql(n.getDay())
-      n.getHours().should.not.be.above(config.shared.order.timelimitH)
-      config.shared.order.weekdays.should.containEql(n.getDay())
+      n.getHours().should.not.be.above(config.shared.order.timelimitH);
+      config.shared.order.weekdays.should.containEql(n.getDay());
     })
     
 
@@ -186,7 +188,7 @@ describe("system.date", function(){
     config.shared.order.timelimitH=1
     var today=new Date();
     delete config.shared.order.weekdays[(today.getDay())]
-    Date.currentShippingDay().getDay().should.equal((today.getDay()+3)%7)      
+    Date.nextShippingDay().getDay().should.equal((today.getDay()+3)%7)      
     done();
   });
 
