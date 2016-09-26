2bd01fc0a97f426d60032793f15e49c2c9889fa8
evaletolab <evaleto@gmail.com>
2016-09-26 21:07:56 +0200

    #71 non admin user can create more than one shop

--- test/products.find.js
+++ test/products.find.js
@@ -78,6 +78,20 @@ describe("products.find:", function(){
     });
   });
 
+  it("Find products by Shop in array", function(done){
+    db.model('Shops').findByUser({"email.address":"evaleto@gluck.com"},function(err,shops){
+      should.exist(shops[0]);
+      console.log('-------------',shops.map(function(shop) {
+        return shop.urlpath;
+      }))
+      Products.findByCriteria({shopname:[shops[0].urlpath]},function(err,products){          
+        products.length.should.equal(2);        
+        products[0].details.comment.should.equal("Temps de cuisson : 16 minutes");
+        done();
+      });
+    });
+  });
+
   it("Find Natural products by Shop  ", function(done){
 
     db.model('Shops').findByUser({"email.address":"evaleto@gluck.com"},function(err,shops){
